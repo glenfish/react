6e0b8a87e633a395e1a3d1513a7717131bc419a8
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2022-08-27 15:24:26 +0700

    added state change and page update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 
 import './App.css';
 import Employee from './components/Employee'
+import {useState} from 'react';
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div className="App">
+      <p>----------</p>
       {showEmployees ?   
       <>
+      <input type='text' onChange={
+          (e) => {
+            
+            setRole(e.target.value);
+            console.log(role)
+          }
+        }></input>
         <Employee name="Nick" role="Intern"/>
-        <Employee name="Abby" role=""/>
+        <Employee name="Abby" role={role}/>
         <Employee name="Scott" role="CTO"/>
       </>
        : <h3>You cannot see the employees, sorry!</h3>}
